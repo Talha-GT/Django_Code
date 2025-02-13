5cd2f3f9051abcffcfe3d60e6e2bd4a26f806eca
Md.Talha Taseen <150540113+Talha-GT@users.noreply.github.com>
2025-02-13 11:38:59 +0600

    Delete task_management directory
    ji

--- task_management/category/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from task.models import TaskModel
-
-class CategoryModel(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
--- task_management/task/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import TaskModel
-
-admin.site.register(TaskModel)
